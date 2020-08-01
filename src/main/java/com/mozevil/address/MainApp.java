f780baf445df737871c3b6c2ad1fcd24927e0238
Maksim Mozzhevilov <mozEvil@users.noreply.github.com>
2020-08-01 19:38:07 +0500

    check up

--- src/main/java/com/mozevil/address/MainApp.java
+++ src/main/java/com/mozevil/address/MainApp.java
@@ -43,15 +43,15 @@ public class MainApp extends Application {
 
     public MainApp() {
         // В качестве образца добавляем некоторые данные
-//        personData.add(new Person("Hans", "Muster"));
-//        personData.add(new Person("Ruth", "Mueller"));
-//        personData.add(new Person("Heinz", "Kurz"));
-//        personData.add(new Person("Cornelia", "Meier"));
-//        personData.add(new Person("Werner", "Meyer"));
-//        personData.add(new Person("Lydia", "Kunz"));
-//        personData.add(new Person("Anna", "Best"));
-//        personData.add(new Person("Stefan", "Meier"));
-//        personData.add(new Person("Martin", "Mueller"));
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
     }
 
     public static void main(String[] args) {
